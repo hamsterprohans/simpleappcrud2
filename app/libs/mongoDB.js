493de38ae0b07254eefaecd938eb9ca363685376
hanscakrawangsa15 <hans.cakrawangsa@gmail.com>
2026-05-13 23:20:56 +0700

    fix: set correct Cosmos DB database name to kel6-database

--- app/libs/mongoDB.js
+++ app/libs/mongoDB.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 const connectMongoDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URL, { tls: true });
+    await mongoose.connect(process.env.MONGODB_URL, {
+      tls: true,
+      dbName: process.env.MONGODB_DB_NAME || "kel6-database",
+    });
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("Error connecting to MongoDB", error);
